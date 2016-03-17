76bd78546c013081e163707e5f44a979aa71b9d2
lizarusi <lizarusi@gmail.com>
2016-03-17 15:17:39 +0200

    Finished task 5

--- interfaceWrapper/en/framework.js
+++ interfaceWrapper/en/framework.js
@@ -8,26 +8,33 @@ var context = {
   module: {},
   console: console,
   // Forward link to fs API into sandbox
-  fs: cloneInterface(fs);
+  fs: cloneInterface(fs)
 };
-//Added clone interface function
-function cloneInterface(anInterface) {
-  var clone = {};
-  for (var key in anInterface) {
-    clone[key] = wrappFunction(key, anInterface[key]);
-  }
-  return clone;
-}
 //Added wrapper for function
 function wrapFunction(fnName, fn) {
   return function wrapper() {
     var args = [];
     Array.prototype.push.apply(args, arguments);
+    Array.prototype.last = function(){
+      return this[this.length - 1]
+    }
     console.log('Call: ' + fnName);
     console.dir(args);
+    if (typeof(args.last()) === 'function'){
+        args[args.length - 1] = wrapFunction('callback', args.last())
+    }
     return fn.apply(undefined, args);
   }
 }
+//Added clone interface function
+function cloneInterface(anInterface) {
+  var clone = {};
+  for (var key in anInterface) {
+    clone[key] = wrapFunction(key, anInterface[key]);
+  }
+  return clone;
+}
+
 // Turn hash into context
 context.global = context;
 var sandbox = vm.createContext(context);
