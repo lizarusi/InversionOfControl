a5dac9b4c6f00cfcd410a784c3364b4c210a2a06
lizarusi <lizarusi@gmail.com>
2016-03-17 14:58:49 +0200

    Finished task 2

--- interfaceWrapper/en/application.js
+++ interfaceWrapper/en/application.js
@@ -1,10 +1,7 @@
 // Print something
 console.log('From application global context');
-
-// Declare function for timer event
-function timerEvent() {
-  console.log('From application timer event');
-}
-
-// Create timer
-setTimeout(timerEvent, 1000);
+var fileName = './README.md';
+console.log('Application going to read ' + fileName);
+fs.readFile(fileName, function(err, src) {
+  console.log('File ' + fileName + ' size ' + src.length);
+});
--- interfaceWrapper/en/framework.js
+++ interfaceWrapper/en/framework.js
@@ -8,23 +8,7 @@ var context = {
   module: {},
   console: console,
   // Forward link to fs API into sandbox
-  fs: fs,
-  // Wrapper for setTimeout in sandbox
-  setTimeout: function(callback, timeout) {
-    // Logging all setTimeout calls
-    console.log(
-      'Call: setTimeout, ' +
-      'callback function: ' + callback.name + ', ' +
-      'timeout: ' + timeout
-    );
-    setTimeout(function() {
-      // Logging timer events before application event
-      console.log('Event: setTimeout, before callback');
-      // Calling user-defined timer event
-      callback();
-      console.log('Event: setTimeout, after callback');
-    }, timeout);
-  }
+  fs: fs
 };
 
 // Turn hash into context
