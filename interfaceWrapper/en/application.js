dcdfe4dfa9a9cf4d4c922a90a770b1fd86253c34
lizarusi <lizarusi@gmail.com>
2016-03-31 14:38:31 +0300

    Finished task6, trying to call write function from fs

--- interfaceWrapper/en/application.js
+++ interfaceWrapper/en/application.js
@@ -1,7 +1,10 @@
 // Print something
 console.log('From application global context');
 var fileName = './README.md';
-console.log('Application going to read ' + fileName);
-fs.readFile(fileName, function(err, src) {
-  console.log('File ' + fileName + ' size ' + src.length);
+console.log('Application going to write ' + fileName);
+// fs.readFile(fileName, function(err, src) {
+//   console.log('File ' + fileName + ' size ' + src.length);
+// });
+fs.writeFile('message.txt', 'Hello Node.js', function(err) {
+  if (err) throw err;
 });
